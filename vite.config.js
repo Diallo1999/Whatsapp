b2fd275aeebb57946e49d944d4d0a3e43a1fd4e2
abdoulaye-99 <layeprof221@gmail.com>
2025-06-12 01:11:27 +0000

    OK

--- vite.config.js
+++ vite.config.js
@@ -9,8 +9,15 @@ export default defineConfig({
         main: './index.html',
         login: './login.html',
         register: './register.html'
+      },
+      output: {
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
       }
-    }
+    },
+    assetsDir: 'assets',
+    cssCodeSplit: false
   },
   server: {
     port: 5173,
